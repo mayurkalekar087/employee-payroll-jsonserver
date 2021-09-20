784cdd24fde529b4ea06ed9dd089dcd0650194a6
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-09-20 18:11:27 +0530

    feat:Ability To Create Emp Payroll Object On Save

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -49,8 +49,10 @@ class EmployeePayrollData{
     toString()
     {
         const options = { year: 'numeric', month: 'long', day:'numeric'};
-        const empDate = this.startDate ? "undefined" :
+        const empDate = !this.startDate ? "undefined" :
                          this.startDate.toLocaleDateString("en-US", options);
-        return "id=" + this.id +" , name=" + this.name + ", gender='" + this.gender + ", profilePic='" + this.profilePic + ", department='" + this.department + ", salary='" + this.salary  + " ,startDate='" + empDate + ", note='" + this.note;
+        return "id=" + this.id +" , name=" + this.name + ", gender='" + this.gender +
+                    ", profilePic='" + this.profilePic + ", department='" + this.department + 
+                    ", salary='" + this.salary  + " ,startDate='" + empDate + ", note='" + this.note;
     }
 }
